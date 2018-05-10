c49b0134234974a941fcf7b8f470274bbfb31e3b
Douglas Gordon <douglas.gordon@vroom.com>
2018-05-09 22:06:19 -0400

    add multi-mutate function

--- src/mutation.js
+++ src/mutation.js
@@ -5,3 +5,10 @@ const mutate = grid => {
   const grid[randomX][randomY] = hexValue;
   return grid;
 };
+
+const mutateXTimes = (grid, mutations) => {
+  for (let i = 0; i < mutations; i += 1) {
+    grid = mutate(grid);
+  }
+  return grid;
+};
